7efe52c5db0f445935a6e538bb97fc917b047290
tetsuya693 <46012616+tetsuya693@users.noreply.github.com>
2019-02-01 12:52:11 +0800

    latest

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -4,7 +4,7 @@ $(aws ecr get-login --no-include-email --registry-ids 204065533127 --region ap-n
 
 if [ `docker images | grep tetsu | wc -l`  = 1 ]
 then
-        docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tetsu
+        docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tetsu:latest
         docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tetsu:latest
 else
         docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tetsu:latest
